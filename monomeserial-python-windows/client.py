49dfcbaf1e4264c63a236df989b9e4b6325aa9fb
dinchak <dinchak@4c0ab224-8a7d-11dd-a14b-9ba93c563a2f>
2010-12-04 19:34:45 +0000

    a number of fixes: row handling, rebinding osc handlers, and more

--- monomeserial-python-windows/client.py
+++ monomeserial-python-windows/client.py
@@ -1,13 +1,34 @@
 import OSC
 import time
 
-serverAddr = "localhost", 8080
+serverAddr = "192.168.1.103", 8080
 client = OSC.OSCClient()
 client.connect(serverAddr)
 
 # send a "/sys/cable 0 top" message
 msg = OSC.OSCMessage()
-msg.setAddress("/sys/cable")
-msg.append(1);
-msg.append("right");
+msg.setAddress("/sys/prefix")
+msg.append(1)
+msg.append("/7up");
 client.send(msg)
+
+#msg = OSC.OSCMessage()
+#msg.setAddress("/sys/offset")
+#msg.append(0);
+#msg.append(0);
+#msg.append(8)
+#client.send(msg)
+
+msg = OSC.OSCMessage()
+msg.setAddress("/sys/cable")
+msg.append(1)
+msg.append("left")
+client.send(msg)
+
+#msg = OSC.OSCMessage()
+#msg.setAddress("/testprefix/led")
+#msg.append(0)
+#msg.append(0)
+#msg.append(1)
+#client.send(msg)
+
